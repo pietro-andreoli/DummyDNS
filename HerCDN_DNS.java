a6b739736864b2899aaaaea75c41cc737bd14596
JacobKlimach <jacobklimach@gmail.com>
2016-12-02 22:27:44 -0500

    Add files via upload

--- HerCDN_DNS.java
+++ HerCDN_DNS.java
@@ -2,14 +2,14 @@ import java.net.*;
 
 public class HerCDN_DNS implements Runnable
 {
-	private static DatagramSocket herContentDomainUDP;
+	private DatagramSocket herContentDomainUDP;
 	
 	public HerCDN_DNS(InetAddress addr, int port) throws Exception
 	{
 		herContentDomainUDP= new DatagramSocket(port,addr);
 	}
 	
-	static public DatagramSocket getUDPSocket()
+	public DatagramSocket getUDPSocket()
 	{
 		return herContentDomainUDP;
 	}
@@ -22,21 +22,10 @@ public class HerCDN_DNS implements Runnable
 					byte[] receiveData = new byte[1024];
 					DatagramPacket rcvPkt = new DatagramPacket(receiveData, receiveData.length);
 					this.herContentDomainUDP.receive(rcvPkt);
-					
 					try
 					{	
-						int receivePort = ClientLocalDNS.getUDPSocket().getLocalPort();
-						InetAddress receiveIP = rcvPkt.getAddress();
-						
 						byte[] data = rcvPkt.getData();
-						byte[] output = analyzeMessage(data);
-                        
-                        DatagramPacket sndPkt = new DatagramPacket(output, output.length, receiveIP, receivePort);
-                        
-                        System.out.println("herCDN.com DNS is replying to Client Local DNS with the type A request on IP address: " + sndPkt.getAddress() + " on Port: " + sndPkt.getPort());
-                        herContentDomainUDP.connect(sndPkt.getSocketAddress());
-                        herContentDomainUDP.send(sndPkt);
-                       
+						System.out.println(new String(data));	
 					}
 					catch(Exception e)
 					{
@@ -48,30 +37,4 @@ public class HerCDN_DNS implements Runnable
 			System.out.println(e);
 		}
 	}
-	
-	public byte[] analyzeMessage(byte[] data)
-	{
-        byte[] outputData = null;
-        String dataS = new String(data);
-        String[] dataChunks = dataS.split("\n");
-        //String[] dataChunks = dataS.split(",");
-        String[][] dataParts = new String[dataChunks.length][];
-
-        for(int i = 0; i < dataChunks.length; i++)
-        {
-            dataParts[i] = dataChunks[i].split(",");
-        }
-        
-        String recordType = dataParts[0][2];
-        
-        if(recordType.contains("A"))
-        {
-            //A type
-        }
-        else if(recordType.contains("V"))
-        {
-            outputData = (dataParts[0][0]+" , " + HerCDNWebServer.getTCPSocket().getInetAddress() + ", A)").getBytes();
-        }
-        return outputData;
-    }
 }
